31654b33e1f72e9c5b1d061dc02754f638d0c4d1
tlh22 <thancock@mhtc.co.uk>
2021-02-02 21:35:46 +0100

    Inlcude config file

--- TOMsExport.py
+++ TOMsExport.py
@@ -75,14 +75,17 @@ import datetime
 from TOMs.core.TOMsGeometryElement import ElementGeometryFactory
 from TOMs.core.TOMsMessageLog import TOMsMessageLog
 from TOMs.generateGeometryUtils import generateGeometryUtils
-from TOMs.restrictionTypeUtilsClass import RestrictionTypeUtilsMixin
+from TOMs.restrictionTypeUtilsClass import RestrictionTypeUtilsMixin, TOMsLayers, TOMsConfigFile
 from TOMsExport.checkableMapLayerList import checkableMapLayerListCtrl, checkableMapLayerList
+#from .restrictionTypeUtilsClass import RestrictionTypeUtilsMixin, TOMsLayers, TOMsConfigFile
 
+"""
 class setupTableNames(QObject):
 
     TOMsLayersNotFound = pyqtSignal()
-    """ signal will be emitted with there is a problem with opening TOMs - typically a layer missing """
-
+"""
+""" signal will be emitted with there is a problem with opening TOMs - typically a layer missing """
+"""
     def __init__(self, iface):
         QObject.__init__(self)
         self.iface = iface
@@ -139,7 +142,9 @@ class setupTableNames(QObject):
                     QMessageBox.information(self.iface.mainWindow(), "ERROR", ("Table " + layer + " is not present"))
                     found = False
                     break
+"""
 
+""" ***
             if QgsProject.instance().mapLayersByName("Bays"):
                 self.BAYS = QgsProject.instance().mapLayersByName("Bays")[0]
             else:
@@ -175,26 +180,20 @@ class setupTableNames(QObject):
             else:
                 QMessageBox.information(self.iface.mainWindow(), "ERROR", ("Table ParkingTariffAreas is not present"))
                 found = False
-
+"""
+"""
         # TODO: need to deal with any errors arising ...
 
         if found == False:
             self.TOMsLayersNotFound.emit()
 
         return self.TOMsExportLayerList
-
+"""
 
 class TOMsExport:
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
-        """Constructor.
-
-        :param iface: An interface instance that will be passed to this class
-            which provides the hook by which you can manipulate the QGIS
-            application at run time.
-        :type iface: QgsInterface
-        """
         # Save reference to the QGIS interface
         self.iface = iface
         # initialize plugin directory
@@ -223,33 +222,10 @@ class TOMsExport:
 
         self.closeTOMs = False
 
-        # Set up log file and collect any relevant messages
-        """logFilePath = os.environ.get('QGIS_LOGFILE_PATH')
-
-        if logFilePath:
-
-            TOMsMessageLog.logMessage("LogFilePath: " + str(logFilePath), level=Qgis.Info)
-
-            logfile = 'qgis_' + datetime.date.today().strftime("%Y%m%d") + '.log'
-            self.filename = os.path.join(logFilePath, logfile)
-            TOMsMessageLog.logMessage("Sorting out log file" + self.filename, level=Qgis.Info)
-            QgsApplication.instance().messageLog().messageReceived.connect(self.write_log_message)"""
-
-    # noinspection PyMethodMayBeStatic
     def tr(self, message):
         """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
         """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('TOMsExport', message)
-
 
     def add_action(
         self,
@@ -315,57 +291,70 @@ class TOMsExport:
 
         # Create the dialog with elements (after translation) and keep reference
         # Only create GUI ONCE in callback, so that it will only load when the plugin is started
-        utils = TOMsExportUtils(self.iface)
-        if self.first_start == True:
-            self.first_start = False
 
-            self.tableNames = setupTableNames(self.iface)
-            self.tableNames.TOMsLayersNotFound.connect(self.setCloseTOMsFlag)
-            #self.TOMsExportLayerList = self.tableNames.getLayers()
-            requiredFields = self.tableNames.getRequiredFields()
+        self.TOMsConfigFileObject = TOMsConfigFile(self.iface)
+        self.TOMsConfigFileObject.TOMsConfigFileNotFound.connect(self.setCloseTOMsFlag)
+        self.TOMsConfigFileObject.initialiseTOMsConfigFile()
 
-            if self.closeTOMs:
-                QMessageBox.information(self.iface.mainWindow(), "ERROR", ("Unable to start TOMs ..."))
-                return
+        utils = TOMsExportUtils(self.iface, self.TOMsConfigFileObject)
 
-            self.setupUi()
+        #if self.first_start == True:
+        #    self.first_start = False
 
-            # show the dialog
-            self.dlg.show()
-            # Run the dialog event loop
-            result = self.dlg.exec_()
-            # See if OK was pressed
-            # TODO: Make sure that there is a valid file location
+        self.tableNames = TOMsLayers(self.iface)
+        #self.tableNames = setupTableNames(self.iface)
+        self.tableNames.TOMsLayersNotFound.connect(self.setCloseTOMsFlag)
+        #self.TOMsExportLayerList = self.tableNames.getLayers()
+        #requiredFields = self.tableNames.getRequiredFields()
 
-            if result:
+        self.tableNames.TOMsLayersNotFound.connect(self.setCloseTOMsFlag)
+        self.tableNames.getLayers(self.TOMsConfigFileObject)
 
-                # Open geopackage
+        if self.closeTOMs:
+            QMessageBox.information(self.iface.mainWindow(), "ERROR", ("Unable to start TOMs ..."))
+            return
 
-                canvas = self.iface.mapCanvas()
-                layer = canvas.currentLayer()
+        #self.TOMsExportLayerList = utils.getTOMsExportLayerList()  # not required as list comes from dialog
+        # TODO: Check that export layers are present ...
 
-                fileName = self.fileNameWidget.filePath()
-                # QMessageBox.information(self.iface.mainWindow(), "Message", ("Filename is ..." + str(fileName)))
-                TOMsMessageLog.logMessage("Filename is ..." + str(fileName), level=Qgis.Info)
+        self.setupUi()
 
-                # Get list of all the layers that are required within the Geopackage
+        # show the dialog
+        self.dlg.show()
+        # Run the dialog event loop
+        result = self.dlg.exec_()
+        # See if OK was pressed
+        # TODO: Make sure that there is a valid file location
 
-                layerItemsList = self.layerList.getSelectedLayers()
-                for currLayerItem in layerItemsList:
-                    TOMsMessageLog.logMessage("Processing {} ...".format(currLayerItem.text()), level=Qgis.Info)
+        if result:
 
-                    currLayer = QgsProject.instance().mapLayersByName(currLayerItem.text())[0]
+            # Open geopackage
 
-                    outputLayersList = utils.processLayer(currLayer, requiredFields)
-                    #status = utils.saveOutputLayers(outputlayersList, fileName)
-                    for newLayerName, newLayer in outputLayersList:
-                        utils.saveLayerToGpkg(newLayer, fileName)
+            canvas = self.iface.mapCanvas()
+            layer = canvas.currentLayer()
 
-                        newLayerA = QgsVectorLayer(fileName + "|layername=" + newLayerName, newLayerName,
-                                                  "ogr")
-                        QgsProject.instance().addMapLayer(newLayerA)
+            fileName = self.fileNameWidget.filePath()
+            # QMessageBox.information(self.iface.mainWindow(), "Message", ("Filename is ..." + str(fileName)))
+            TOMsMessageLog.logMessage("Filename is ..." + str(fileName), level=Qgis.Info)
 
-                TOMsMessageLog.logMessage("******** FINSIHED ********", level=Qgis.Info)
+            # Get list of all the layers that are required within the Geopackage
+
+            layerItemsList = self.layerList.getSelectedLayers()
+            for currLayerItem in layerItemsList:
+                TOMsMessageLog.logMessage("Processing {} ...".format(currLayerItem.text()), level=Qgis.Warning)
+
+                currLayer = QgsProject.instance().mapLayersByName(currLayerItem.text())[0]
+
+                outputLayersList = utils.processLayer(currLayer)
+                #status = utils.saveOutputLayers(outputlayersList, fileName)
+                for newLayerName, newLayer in outputLayersList:
+                    utils.saveLayerToGpkg(newLayer, fileName)
+
+                    newLayerA = QgsVectorLayer(fileName + "|layername=" + newLayerName, newLayerName,
+                                              "ogr")
+                    QgsProject.instance().addMapLayer(newLayerA)
+
+            TOMsMessageLog.logMessage("******** FINSIHED ********", level=Qgis.Info)
 
             #self.dlg.close()
 
@@ -415,19 +404,53 @@ class TOMsExport:
 
 class TOMsExportUtils():
 
-    def __init__(self, iface):
+    def __init__(self, iface, configFileObject):
         self.iface = iface
+        self.configFileObject = configFileObject
 
-    def processLayer(self, currLayer, requiredFields):
+    def getTOMsExportLayerList(self):
+        layers = self.configFileObject.getTOMsConfigElement('TOMsExport', 'LayersToExport')
+        if layers:
+            exportLayerList = layers.split('\n')
+            return exportLayerList
+        return None
 
-        #currFields = currLayer.fields()
+    def getOnlyCurrentFeatures(self):
+        value = self.configFileObject.getTOMsConfigElement('TOMsExport', 'GetOnlyCurrentFeatures')
+        if value == 'True':
+            return True
+        return False
 
-        # decide whether or not this is a TOMs layer.
-        fieldsToInclude = self.setFieldsForTOMsExportLayer(currLayer, requiredFields)
+    def exportBaysAsPolygons(self):
+        value = self.configFileObject.getTOMsConfigElement('TOMsExport', 'BaysAsPolygons')
+        if value == 'True':
+            return True
+        return False
+
+    def getFieldsForExportLayer(self, layerName):
+        configName = layerName + '.Fields'
+        fields = self.configFileObject.getTOMsConfigElement('TOMsExport', configName)
+        if fields:
+            layerFieldList = fields.split('\n')
+            return layerFieldList
+
+        return None
+
+    def processLayer(self, currLayer):
+
+        TOMsMessageLog.logMessage("In processLayer - Considering layer: ".format(currLayer.name()), level=Qgis.Warning)
+
+        # get the fields to include
+        listFieldsToInclude = self.getFieldsForExportLayer(currLayer.name())
+        TOMsMessageLog.logMessage("In processLayer - fields are {}".format(listFieldsToInclude), level=Qgis.Warning)
+
+        # take string and turn into fields
+        fieldsToInclude = self.setFieldsForTOMsExportLayer(currLayer, listFieldsToInclude)
 
         if len(fieldsToInclude) == 0:
             return None  # no details to add
 
+        # decide whether or not to use only current restrictions.
         if self.isThisTOMsLayer(currLayer) == True:
             text = '"OpenDate" IS NOT NULL AND "CloseDate" IS NULL'
             exp = QgsExpression(text)
@@ -436,15 +459,26 @@ class TOMsExportUtils():
         else:
             restrictionIterator = currLayer.getFeatures()
 
-        TOMsMessageLog.logMessage("In processLayer - " + currLayer.name(), level=Qgis.Info)
-
         layerWkbType = currLayer.wkbType()
         outputLayersList = []
         #restrictionList = currLayer.getFeatures(request)
 
         for currRestriction in restrictionIterator:
             TOMsMessageLog.logMessage("In processLayer - geomID: {}".format(currRestriction.attribute("GeometryID")),
-                                     level=Qgis.Info)
+                                     level=Qgis.Warning)
+
+            # deal with Bays as polygons ...
+            if currLayer.name() == "Bays":
+                if self.exportBaysAsPolygons() == True:
+                    currGeomShapeID = currRestriction.attribute("GeomShapeID")
+                    if currGeomShapeID < 20:
+                        TOMsMessageLog.logMessage(
+                            "In processLayer - Changing geomShapeID from {} to {}".format(currGeomShapeID, currGeomShapeID + 20),
+                            level=Qgis.Warning)
+
+                        currRestriction[currRestriction.fields().indexFromName("GeomShapeID")] = currGeomShapeID + 20
+                        #currRestriction.setAttribute("GeomShapeID", currGeomShapeID+20)
+
             restrictionGeometryWkbType = self.getRestrictionGeometryWkbType(currRestriction, layerWkbType)
             # check that we have a layer created
             outputLayerName = '{currLayerName}_{ext}'.format(currLayerName=currLayer.name(), ext=QgsWkbTypes.displayString(restrictionGeometryWkbType))
@@ -464,7 +498,10 @@ class TOMsExportUtils():
             result = self.processRestriction(currRestriction, outputLayer)
             if result == False:
                 reply = QMessageBox.information(None, "Information", "failure to write to layer.", QMessageBox.Ok)
-                break
+                TOMsMessageLog.logMessage(
+                    "In processLayer: failure to write to layer ...",
+                    level=Qgis.Info)
+                ##break
 
         for newLayerName, newLayer in outputLayersList:
             newLayer.reload()
@@ -520,6 +557,10 @@ class TOMsExportUtils():
     def isThisTOMsLayer(self, currLayer):
         # Decide whether or not this is a TOMs layer.
         # Look for "OpenDate" and check whether or not there are values set
+
+        if self.getOnlyCurrentFeatures() == False:
+            return False
+
         isTOMsLayer = False
         currFields = currLayer.fields()
 
@@ -598,11 +639,16 @@ class TOMsExportUtils():
                 newFeature.setAttribute(field.name(), currRestriction.attribute(field.name()))
 
         #newGeom = currRestriction.geometry()  # use this for testing
+
+
         newGeom = ElementGeometryFactory.getElementGeometry(currRestriction)
 
         newFeature.setGeometry(newGeom)
 
         result = newLayer.dataProvider().addFeature(newFeature)
+        if result == False:
+            TOMsMessageLog.logMessage("In processRestriction. Error: {}".format(newLayer.dataProvider().errors()),
+                                      level=Qgis.Warning)
         return result
 
 class checkableMapLayerListCtrl:
